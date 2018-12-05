6301cb27dda22f323aff9bd43d9da58a45c6f2f5
rjlancaster <rjlancaster@gmail.com>
2018-12-05 17:22:31 -0600

    working fetch to displaying poster images in recommendations view

--- src/components/recs/RecsList.js
+++ src/components/recs/RecsList.js
@@ -1,46 +1,39 @@
 import React, { Component } from "react"
-import DataManager from "../../module/DataManager"
+// import DataManager from "../../module/DataManager"
 
 export default class RecsList extends Component {
   state = {
     images: []
-
   }
 
   componentDidMount() {
     let images = []
     this.props.shows.map(show => {
-
       const url = `https://api.themoviedb.org/3/tv/${show.apiID}?api_key=71beceaec7947e27f4fa92aadc09db8c`
       return fetch(url)
         .then(data => data.json())
         .then(data => {
-         let showItem = data.poster_path;
+          let showItem = data.poster_path;
           let movieImageUrl = `https://image.tmdb.org/t/p/w500${showItem}`
           images.push(movieImageUrl)
-          this.setState(images)
+          this.setState({images: images})
         })
-
-
-  })
+    })
   }
-
-
 
   render() {
     return (
       <section className="recs">
-      {
+        {
           this.state.images.map(image => {
             // console.log(this.showlist(shows.apiID))
-              return (<div>
-              <p> Hello </p>
-                <img src={image} alt="hi"/>
-              </div>
-              )
-              }
+            return (<div>
+              <img src={image} alt="tv-poster" />
+            </div>
+            )
+          }
           )
-      }
+        }
       </section>
     )
   }
