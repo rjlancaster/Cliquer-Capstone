7d7d3cd30479ea680c21200377f9866a03486d2e
rjlancaster <rjlancaster@gmail.com>
2018-12-13 12:19:22 -0600

    video function on search, recommendations and history views

--- src/components/detail/detailsModal.js
+++ src/components/detail/detailsModal.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Modal, ModalBody, ModalFooter } from 'reactstrap';
 import { Button } from 'semantic-ui-react'
+import YouTube from 'react-youtube'
 import "./detail.css"
 import ApiManager from "../../module/ApiManager"
 
 export default class DetailsModal extends React.Component {
   credentials = JSON.parse(sessionStorage.getItem('credentials'))
+  state = {
+    showVideo: []
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      showVideo: {}
     };
 
     this.toggle = this.toggle.bind(this);
@@ -48,13 +54,34 @@ export default class DetailsModal extends React.Component {
       })
   }
 
+  constructVideoSearch = evt => {
+    const url = `https://api.themoviedb.org/3/tv/${this.props.show.apiID}/videos?api_key=71beceaec7947e27f4fa92aadc09db8c&language=en-US`
+    return fetch(url)
+      .then(data => data.json())
+      .then(data => {
+        let showVideoID = {
+          videoID: data.results[0].key
+        }
+        this.setState({ showVideo: showVideoID })
+      })
+  }
+
   toggle() {
     this.setState({
       modal: !this.state.modal
     });
+    this.constructVideoSearch()
   }
 
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 0
+      }
+    }
     return (
       <div>
         <div className="posterImageDiv">
@@ -63,8 +90,16 @@ export default class DetailsModal extends React.Component {
         <Modal className="modal-container" size="xl" isOpen={this.state.modal} toggle={this.toggle} >
           <ModalBody>
             <div className="detail-group">
-              <div>
-                <img className="detailImage" src={`https://image.tmdb.org/t/p/w300${this.props.show.image}`} alt="tv-poster" />
+              <div className="image">
+                <div className="taco">
+                  <img className="detailImage" src={`https://image.tmdb.org/t/p/w300${this.props.show.image}`} alt="tv-poster" />
+                  <YouTube
+                    className="detailImage"
+                    videoId={this.state.showVideo.videoID}
+                    opts={opts}
+                    onReady={this._onReady}
+                  />
+                </div>
               </div>
               <div className="titleAndSynopsis">
                 <h4>{this.props.show.title}</h4>
